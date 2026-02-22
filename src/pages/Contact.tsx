25fe31bc59414c1a75aed99a1dd98b4462d56f88
Kumar95016 <139850500+Kumar95016@users.noreply.github.com>
2026-02-22 18:58:30 +0530

    Updated maps

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -302,7 +302,7 @@ const Contact = () => {
       {/* Map Section */}
       <section className="h-96 bg-muted">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3610.5329556376!2d55.26326!3d25.18682!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f682829c85c07%3A0xa5eda9fb3c93b69d!2sBusiness%20Bay%20-%20Dubai!5e0!3m2!1sen!2sae!4v1635000000000!5m2!1sen!2sae"
+          src="https://maps.google.com/maps?q=Airwings+Tour+And+Travels+LLC,+Al+Sharhan+Apartments+-+Office+no:5+-+5th+St+-+Al+Nahda:2+-+Al+Nahda+-+Dubai&output=embed"
           width="100%"
           height="100%"
           style={{ border: 0 }}
